dbfffd691b9241ca5264de801df5cf2e3bcd17a2
moxiao <moxiao@mogujie.com>
2016-08-21 01:20:15 +0800

    oop

---
+++ oop_pk/person_start.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: mx
+@software: PyCharm
+@file: person_start.py
+@time: 16/8/21 01:12
+"""
+
+
+class Person:
+    def __init__(self, name, age, pay=0, job=None):
+        self.name = name
+        self.age = age
+        self.pay = pay
+        self.job = job
+
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith', 42, 30000, 'software')
+    sue = Person('Sue Jones', 45, 40000, 'hardware')
+    print(bob.name, sue.pay)
+
+    print(bob.name.split()[-1])
+    sue.pay *= 1.10
+    print(sue.pay)
