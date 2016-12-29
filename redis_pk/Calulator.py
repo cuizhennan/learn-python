c07d7b6d8b37ed36380229d51a810ef21d654874
moxiao <moxiao@mogujie.com>
2016-12-29 09:51:14 +0800

    python testcase

--- redis_pk/Calulator.py
+++ redis_pk/Calulator.py
@@ -8,13 +8,16 @@
 @time: 16/12/28 16:15
 """
 
+name = "whole global name"
+
 
 class Calulator(object):
     # define class to simulate a simple calculator
 
-    def __init__(self):
+    def __init__(self, newName):
         # start with zero
         self.current = 0
+        self.name = newName
 
     def add(self, amount):
         # add number to current
@@ -22,3 +25,15 @@ class Calulator(object):
 
     def getCurrent(self):
         return self.current
+
+    def sayYourName(self):
+        print('My name is %s' % self.name)
+
+
+def selfAndInitDemo():
+    persionInstance = Calulator('Gavin.')
+    persionInstance.sayYourName()
+
+
+if __name__ == '__main__':
+    selfAndInitDemo()
